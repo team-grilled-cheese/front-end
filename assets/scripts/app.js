cf42e4e4120a6a0d64ce06ae6490d69925ec82da
Scott Solmonson <ssolmonson63@gmail.com>
2020-02-19 18:52:17 -0500

    Add submit survey event
    Created an event to submit survey with the radio buttons. Issue still
    present with pressing the submit button as nothing happens.
    
    Co-authored-by: Isaac isaacr90@icloud.com
    Co-authored-by: Kayla shalisha.angelique@gmail.com

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,8 @@ const surveyEvents = require('./survey/surveyEvents')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+
+// const answerEvents = require('./answer/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
@@ -17,4 +19,5 @@ $(() => {
   authEvents.addHandlers()
   // your JS code goes here
   surveyEvents.addHandlers()
+  // answerEvents.addHandlers()
 })
