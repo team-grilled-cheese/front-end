04560622e380a58ad08b035673ed4002242e8e52
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-02-20 17:09:52 -0500

    Display answers from a survey
    on the click of a survey, get the answers for that survey.
    Compare answers to possible answers from a survey and show how many
    users have selected that answer.

--- assets/scripts/survey/ui.js
+++ assets/scripts/survey/ui.js
@@ -1,6 +1,7 @@
 const store = require('./../store')
 const surveysTemplate = require('../templates/survey-listing.handlebars')
 const showOneSurvey = require('../templates/oneSurvey.handlebars')
+const api = require('./../answer/api')
 
 const onCreateSuccess = function (response) {
   store.survey = response.survey
@@ -32,6 +33,49 @@ const onGetAllSurveysFailure = (response) => {
 const onGetOneSurveySuccess = (response) => {
   const showThisSurvey = showOneSurvey({ survey: response.survey })
   $('#surveybox').html(showThisSurvey)
+  store.survey = response.survey
+  api.getThisSurveyAnswers()
+    .then(showSurveyAnswers)
+    .catch()
+}
+
+const showSurveyAnswers = (response) => {
+  $('.answer1').text(' ')
+  $('.answer2').text(' ')
+  $('.answer3').text(' ')
+  $('.answer4').text(' ')
+  store.answersArray = response.answers
+  const possibleAnswers = []
+  store.survey.possibleAnswers.forEach(answer => {
+    possibleAnswers.push(answer)
+  })
+  const answerArray = []
+  store.answersArray.forEach(answer => {
+    answerArray.push(answer)
+  })
+  const allAnswers = []
+  for (let i = 0; i < answerArray.length; i++) {
+    allAnswers.push(answerArray[i].answer)
+  }
+  const answer1 = []
+  const answer2 = []
+  const answer3 = []
+  const answer4 = []
+  for (let i = 0; i < allAnswers.length; i++) {
+    if (allAnswers[i] === possibleAnswers[0]) {
+      answer1.push(allAnswers[i])
+    } else if (allAnswers[i] === possibleAnswers[1]) {
+      answer2.push(allAnswers[i])
+    } else if (allAnswers[i] === possibleAnswers[2]) {
+      answer3.push(allAnswers[i])
+    } else if (allAnswers[i] === possibleAnswers[3]) {
+      answer4.push(allAnswers[i])
+    }
+  }
+  $('.answer1').text(`${answer1.length}` + ' users have choosen ' + `${possibleAnswers[0]}`)
+  $('.answer2').text(`${answer2.length}` + ' users have choosen ' + `${possibleAnswers[1]}`)
+  $('.answer3').text(`${answer3.length}` + ' users have choosen ' + `${possibleAnswers[2]}`)
+  $('.answer4').text(`${answer4.length}` + ' users have choosen ' + `${possibleAnswers[4]}`)
 }
 
 const onGetOneSurveyFailure = () => {
