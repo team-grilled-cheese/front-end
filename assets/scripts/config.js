a297833087d9af62605f2da0ffbe78d250754828
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-02-21 09:23:35 -0500

    fix heroku link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://blooming-meadow-93930.herokuapp.com/',
+  production: 'https://blooming-meadow-93930.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
